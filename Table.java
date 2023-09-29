c55e55ed3b647b9bbc575dcf1418e4c5e4d6389e
netabirami <net.abirami@gmail.com>
2023-09-29 14:29:17 +0200

    I created Available

---
+++ Table.java
@@ -0,0 +1,12 @@
+public class Table {
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    private boolean isAvailable;
+
+}
