2903688f852ed1a237050f774b66ce6665501303
saivelamuri <saivelamuri26@gmail.com>
2023-09-29 14:33:32 +0200

    commited

---
+++ TableNumber.java
@@ -0,0 +1,18 @@
+public class Order {
+
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    int  tableNumber;
+
+        public static void getDetails(){
+
+        };
+    }
+
+
