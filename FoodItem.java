42de6f8bd299715c4fc189f7830cf51ef94f09e7
hcs <hannahemcs@gmail.com>
2023-09-29 14:26:41 +0200

    Add price attribute incl getter and setter

---
+++ FoodItem.java
@@ -0,0 +1,11 @@
+public class FoodItem {
+    double price;
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
